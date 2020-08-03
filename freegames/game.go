e6804bf7dd733ced851c9aff7f05b0b2a1ce3744
Samuel Porras <samuelporras@gmail.com>
2020-08-03 19:16:30 +0200

    Added repository domain

--- freegames/game.go
+++ freegames/game.go
@@ -1,8 +1,35 @@
 package freegames
 
+import "time"
+
 // Game struct represent a free game
 type Game struct {
-	Name  string
-	Photo string
-	URL   string
+	Name      string    `json:"name"`
+	Photo     string    `json:"photo"`
+	URL       string    `json:"url"`
+	Platform  string    `json:"platform"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at,omitempty"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at,omitempty"`
+}
+
+// GetAllFreeGames get all free games from all injected platforms
+func GetAllFreeGames(pool []Platform, db Repository) []Game {
+	freeGames := make([]Game, 0)
+	for _, v := range pool {
+
+		games, err := v.Run()
+		if err != nil {
+			panic(err)
+		}
+		if len(games) > 0 {
+			for _, g := range games {
+				if !db.Exists(g) {
+					db.Store(g)
+					freeGames = append(freeGames, g)
+				}
+			}
+		}
+	}
+
+	return freeGames
 }
--- freegames/repo.go
+++ freegames/repo.go
@@ -1,1 +1,8 @@
 package freegames
+
+// Repository find and store free games into a database
+type Repository interface {
+	Exists(game Game) bool
+	GetGames() ([]Game, error)
+	Store(games Game) error
+}
