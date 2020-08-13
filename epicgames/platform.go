8702affd2e6f2e073eca7a4531dd062d86de5256
Samuel Porras <samuelporras@gmail.com>
2020-08-13 23:31:37 +0200

    Bug: Fixed #8 issue

--- epicgames/platform.go
+++ epicgames/platform.go
@@ -1,9 +1,11 @@
 package epicgames
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 
@@ -12,6 +14,7 @@ import (
 
 const graphqlURL = "https://www.epicgames.com/store/backend/graphql-proxy"
 const gameInfoURL = "https://store-content.ak.epicgames.com/api/es-ES/content/products/%s"
+const gameFreeGames = "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=es-ES&country=ES&allowCountries=ES"
 
 type epicGames struct{}
 
@@ -26,7 +29,7 @@ func (u *epicGames) Run() ([]freegames.Game, error) {
 	games := make([]freegames.Game, 0, 4)
 
 	jsonData := createQueryFreeGames()
-	request, err := createRequest(jsonData)
+	request, err := createRequest("POST", graphqlURL, bytes.NewBuffer(jsonData))
 	if err != nil {
 		return games, err
 	}
@@ -77,24 +80,27 @@ func (u *epicGames) Run() ([]freegames.Game, error) {
 // IsFree check if a game is currently free or not
 func (u *epicGames) IsFreeGame(game freegames.Game) bool {
 
-	jsonData := createQueryGame(game.OfferID, game.ProductNamespace)
-	request, err := createRequest(jsonData)
+	response, err := http.Get(gameFreeGames)
 	if err != nil {
+		log.Printf("Error ocurried in IsFreeGame method in platform %s: %s\n", u.GetName(), err.Error())
 		return true
 	}
 
-	client := &http.Client{Timeout: time.Second * 10}
-	response, err := client.Do(request)
-	if err != nil {
-		return true
-	}
 	defer response.Body.Close()
 	data, _ := ioutil.ReadAll(response.Body)
 
-	rq := epicgamesGameResponse{}
+	rq := epicgamesResponse{}
 	json.Unmarshal(data, &rq)
 
-	return u.IsFree(rq.Data.Catalog.CatalogOffer.Price)
+	for _, newGame := range rq.Data.Catalog.SearchStore.Elements {
+		if game.Name == newGame.Title {
+			log.Printf("No delete %s game, it's free now.\n", game.Name)
+			return true
+		}
+	}
+
+	log.Printf("Deleted %s game, no longer free.\n", game.Name)
+	return false
 }
 
 // IsFree It's business logic exclusive to the epic games platform that checks whether a game is free
