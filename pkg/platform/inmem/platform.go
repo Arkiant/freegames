be356e106608fba047722feac2640695eec44726
Samuel Porras <samuelporras@gmail.com>
2020-08-20 12:57:29 +0200

    Feature: added fixed games and logic to platform

--- pkg/platform/inmem/platform.go
+++ pkg/platform/inmem/platform.go
@@ -1,6 +1,10 @@
 package inmem
 
-import freegames "github.com/arkiant/freegames/pkg"
+import (
+	"time"
+
+	freegames "github.com/arkiant/freegames/pkg"
+)
 
 // Platform is a not implemented platform used by tests
 type Platform struct{}
@@ -15,7 +19,29 @@ func (u *Platform) Run() ([]freegames.Game, error) {
 
 	games := make([]freegames.Game, 0, 4)
 
-	// TODO: ADD FIXED GAMES
+	games = append(games, freegames.Game{
+		Name:             "Test Free Game",
+		ProductNamespace: "Freegame",
+		CreatedAt:        time.Now(),
+		OfferID:          "free-test-game",
+		Photo:            "blank",
+		Platform:         "InmemPlatform",
+		Slug:             "test-free-game",
+		URL:              "http://localhost/test-free-game",
+		UpdatedAt:        time.Now(),
+	})
+
+	games = append(games, freegames.Game{
+		Name:             "Test No Free Game",
+		ProductNamespace: "NoFreegame",
+		CreatedAt:        time.Now(),
+		OfferID:          "no-free-test-game",
+		Photo:            "blank",
+		Platform:         "InmemPlatform",
+		Slug:             "test-no-free-game",
+		URL:              "http://localhost/test-no-free-game",
+		UpdatedAt:        time.Now(),
+	})
 
 	return games, nil
 
@@ -23,7 +49,12 @@ func (u *Platform) Run() ([]freegames.Game, error) {
 
 // IsFreeGame check if a game is currently free or not
 func (u *Platform) IsFreeGame(game freegames.Game) bool {
-	return true
+
+	if game.Name == "Test Free Game" {
+		return true
+	}
+
+	return false
 }
 
 // GetName Get platform name
