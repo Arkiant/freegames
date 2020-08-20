3aff3c20d3239d55ba5a86f985b4d596346f65c2
Samuel Porras <samuelporras@gmail.com>
2020-08-20 21:52:12 +0200

    #9 Refactor: create custom command errors

--- pkg/command_handler.go
+++ pkg/command_handler.go
@@ -5,6 +5,14 @@ import (
 	"errors"
 )
 
+var (
+	// ErrCommandNotFound when command not found
+	ErrCommandNotFound = errors.New("command not found")
+
+	// ErrCommandExists when command exists
+	ErrCommandExists = errors.New("command exists")
+)
+
 // CommandParams represents parameters passed by context
 type CommandParams string
 
@@ -37,7 +45,7 @@ func (handler CommandHandler) Get(name string) (Command, error) {
 		return v, nil
 	}
 
-	return nil, errors.New("command not found")
+	return nil, ErrCommandNotFound
 }
 
 // Register a client command to be used
@@ -47,7 +55,7 @@ func (handler CommandHandler) Register(name string, cc Command) error {
 		return nil
 	}
 
-	return errors.New("command exists")
+	return ErrCommandExists
 }
 
 // ExecuteCommand with context and name
