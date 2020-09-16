c8f0c70e62e281467ca00718af80860c01be2340
Samuel Porras <samuelporras@gmail.com>
2020-09-16 10:08:20 +0200

    Feature: created abstraction and configuration freegames fields

--- pkg/config.go
+++ pkg/config.go
@@ -1,4 +1,22 @@
 package freegames
 
+// Configuration config structure
+type Configuration struct {
+	Clients []struct {
+		Discord struct {
+			Enable  bool
+			Channel string
+		}
+	}
+	Platform []string
+}
+
 // Config abstraction to decouple and create an adapter to use any configuration library
-type Config interface{}
+type Config interface {
+	ClientConfig() ([]struct {
+		Discord struct {
+			Enable  bool
+			Channel string
+		}
+	}, error)
+}
