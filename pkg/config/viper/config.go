7f1879234a59d83095ebe1a0b21e85dadc0580bb
Samuel Porras <samuelporras@gmail.com>
2020-09-16 10:08:46 +0200

    Feature: created viper implementation configuration

---
+++ pkg/config/viper/config.go
@@ -0,0 +1,37 @@
+package viper
+
+import (
+	"errors"
+
+	freegames "github.com/arkiant/freegames/pkg"
+	"github.com/spf13/viper"
+)
+
+type viperConfiguration struct {
+	freegames.Configuration
+}
+
+// NewViperConfiguration create a new config implementation using viper library
+func NewViperConfiguration(file string) (freegames.Config, error) {
+	viper.SetConfigFile(file)
+
+	configuration := freegames.Configuration{}
+	err := viper.UnmarshalExact(configuration)
+	if err != nil {
+		return nil, err
+	}
+
+	return &viperConfiguration{Configuration: configuration}, nil
+}
+
+func (vc *viperConfiguration) ClientConfig() ([]struct {
+	Discord struct {
+		Enable  bool
+		Channel string
+	}
+}, error) {
+	if len(vc.Configuration.Clients) <= 0 {
+		return nil, errors.New("No clients specified")
+	}
+	return vc.Configuration.Clients, nil
+}
