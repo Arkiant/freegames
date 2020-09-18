90f72117d6442af8bb861ff90cc5dd5f417673d7
Samuel Porras <samuelporras@gmail.com>
2020-09-18 11:08:43 +0200

    Feature: added new discord configuration implementation

--- pkg/client/discord/client_config.go
+++ pkg/client/discord/client_config.go
@@ -1,1 +1,26 @@
 package discord
+
+import freegames "github.com/arkiant/freegames/pkg"
+
+type discordConfiguration struct {
+	token   string
+	channel string
+	prefix  string
+}
+
+// NewDiscordConfiguration contructor
+func NewDiscordConfiguration(token, prefix, channel string) freegames.ClientConfig {
+	return &discordConfiguration{token: token, prefix: prefix, channel: channel}
+}
+
+func (dc *discordConfiguration) GetChannel() string {
+	return dc.channel
+}
+
+func (dc *discordConfiguration) GetToken() string {
+	return dc.token
+}
+
+func (dc *discordConfiguration) GetPrefix() string {
+	return dc.prefix
+}
