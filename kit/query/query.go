ac56108bc9af94a6f381514310fcc275e0c41f4e
Arkiant <samuelporras@gmail.com>
2021-11-22 19:32:40 +0100

    refactor: change comments

--- kit/query/query.go
+++ kit/query/query.go
@@ -2,25 +2,25 @@ package query
 
 import "context"
 
-// Bus defines the expected behaviour from a command bus.
+// Bus defines the expected behaviour from a query bus.
 type Bus interface {
-	// Dispatch is the method used to dispatch new commands.
+	// Dispatch is the method used to dispatch new queries.
 	Dispatch(context.Context, Query) (interface{}, error)
-	// Register is the method used to register a new command handler.
+	// Register is the method used to register a new query handler.
 	Register(Type, Handler)
 }
 
 //go:generate mockery --case=snake --outpkg=querymocks --output=querymocks --name=Bus
 
-// Type represents an application command type.
+// Type represents an application query type.
 type Type string
 
-// Command represents an application command.
+// Query represents an application query.
 type Query interface {
 	Type() Type
 }
 
-// Handler defines the expected behaviour from a command handler.
+// Handler defines the expected behaviour from a query handler.
 type Handler interface {
 	Handle(context.Context, Query) (interface{}, error)
 }
